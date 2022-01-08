fe71083a492d6e6e388ef94893c4883b0cb5d20e
dohpark <parkdohee19931129@gmail.com>
2022-01-09 03:44:36 +0900

    feat: 추가 Image atom
    이미지에 src, alt 속성을 줄 수 있으며, hover할 시에 trasform을 통해 1.1배 확대 가능함

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import Image from "./components/atoms/Image";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">Learn React</header>
+      <Image
+        src="https://cloudfour.com/examples/img-currentsrc/images/kitten-small.png"
+        alt="dfdfdfd"
+        expand={true}
+      />
     </div>
   );
 }
---
+++ src/components/atoms/Image/index.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import styled, { css } from "styled-components";
+
+interface ImageProps extends React.ImgHTMLAttributes<HTMLImageElement> {
+  src: string;
+  alt: string;
+  expand: boolean;
+}
+
+interface StyledImageProps {
+  expand: boolean;
+}
+
+const getExpandState = (expand: boolean) => {
+  return expand
+    ? css`
+        transform: scale(1.1);
+      `
+    : css``;
+};
+
+const ImageBox = styled.img.attrs((props) => ({
+  src: props.src,
+  alt: props.alt,
+}))<StyledImageProps>`
+  max-width: 100%;
+  transition: all 0.3s;
+  display: block;
+  width: 100%;
+  transform: scale(1);
+
+  &:hover {
+    ${(props) => getExpandState(props.expand)};
+  }
+`;
+
+const Image: React.FC<ImageProps> = ({ src, alt, expand }) => {
+  return <ImageBox src={src} alt={alt} expand={expand} />;
+};
+
+export default Image;
