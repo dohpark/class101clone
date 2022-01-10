a781cdecd4170fc2102633a56f3f16b669d19f60
dohpark <parkdohee19931129@gmail.com>
2022-01-10 16:02:41 +0900

    feat: 추가 IconButton atom
    background, size, iconName, fillColor, hover 기능 포함하도록 함

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import Button from "./components/atoms/Button";
 import Icon from "./components/atoms/Icon";
+import IconButton from "./components/atoms/IconButton";
+import palette from "./styles/palette";
+
 function App() {
   return (
     <div className="App">
       <Icon size={100} iconName={"Heart"} />
       <Button>help</Button>
+      <IconButton
+        iconName={"Like"}
+        fillColor={palette.orange400}
+        backgroundColor="transparent"
+      />
     </div>
   );
 }
---
+++ src/components/atoms/IconButton/index.tsx
@@ -0,0 +1,86 @@
+import styled, { css } from "styled-components";
+import palette from "../../../styles/palette";
+import Icon from "../Icon";
+
+type iconName =
+  | "ChevronLeft"
+  | "ChevronRight"
+  | "Close"
+  | "Heart"
+  | "HeartOutline"
+  | "Like"
+  | "LikeOutline";
+
+const getButtonBackgroundColor = (color: string) => {
+  switch (color) {
+    case "white":
+      return css`
+        background-color: ${palette.white};
+      `;
+    case "black":
+      return css`
+        background-color: ${palette.black};
+      `;
+    case "transparent":
+      return css`
+        background-color: transparent;
+      `;
+    default:
+      return css`
+        background-color: transparent;
+      `;
+  }
+};
+
+const getButtonSize = (size: "sm") => {
+  switch (size) {
+    case "sm":
+      return css`
+        height: 32px;
+        width: 32px;
+        margin: 4px;
+      `;
+  }
+};
+
+interface StyledIconButtonProps {
+  size: "sm";
+  backgroundColor: "white" | "transparent" | "black";
+}
+
+const IconButtonBox = styled.button<StyledIconButtonProps>`
+  padding: 0;
+  border: 0;
+  border-radius: 3px;
+  outline: none;
+  cursor: pointer;
+  ${(props) => getButtonSize(props.size)};
+  ${(props) => getButtonBackgroundColor(props.backgroundColor)}
+
+  &:hover {
+    background-color: rgba(255, 255, 255, 0.1);
+  }
+`;
+
+interface IconButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  backgroundColor?: "white" | "transparent" | "black";
+  size?: "sm";
+  iconName: iconName;
+  fillColor: string;
+}
+
+const IconButton: React.FC<IconButtonProps> = ({
+  backgroundColor = "transparent",
+  size = "sm",
+  iconName,
+  fillColor,
+}) => {
+  return (
+    <IconButtonBox backgroundColor={backgroundColor} size={size}>
+      <Icon size={18} iconName={iconName} fillColor={fillColor} />
+    </IconButtonBox>
+  );
+};
+
+export default IconButton;
