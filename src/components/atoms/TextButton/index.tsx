f793d021fc0fe8efd37ecbedb232cd8b169bbb23
dohpark <parkdohee19931129@gmail.com>
2022-01-12 01:00:47 +0900

    feat: 추가 TextButton atom
    텍스트 버튼 새로 추가함. hover 시 텍스트에 밑줄 생기며, margin, padding, border이 없는것이 다른 버튼과의 차이점

---
+++ src/components/atoms/TextButton/index.tsx
@@ -0,0 +1,81 @@
+import styled, { css } from "styled-components";
+import palette from "../../../styles/palette";
+
+const getButtonColor = (color: string) => {
+  switch (color) {
+    case "orange":
+      return css`
+        color: ${palette.orange500};
+      `;
+    case "orangeLight":
+      return css`
+        color: ${palette.orange100};
+      `;
+    default:
+      return css`
+        color: ${palette.gray600};
+      `;
+  }
+};
+
+const getButtonSize = (size: "md" | "lg") => {
+  switch (size) {
+    case "md":
+      return css`
+        font-size: 14px;
+        font-weight: 500;
+      `;
+    case "lg":
+      return css`
+        font-size: 16px;
+        font-weight: 600;
+      `;
+  }
+};
+
+interface StyledTextButtonProps {
+  color: "default" | "orange" | "orangeLight";
+  size: "md" | "lg";
+}
+
+const TextButtonBox = styled.button<StyledTextButtonProps>`
+  border: 0px;
+  outline: none;
+  margin: 0px;
+  padding: 0;
+  background: none;
+  cursor: pointer;
+  letter-spacing: -0.2px;
+
+  ${(props) => getButtonSize(props.size)}
+  ${(props) => getButtonColor(props.color)}
+
+  &:hover {
+    text-decoration: underline;
+    filter: brightness(85%);
+  }
+`;
+
+const TextButtonSpan = styled.span``;
+
+interface TextButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+  color?: "default" | "orange" | "orangeLight";
+  size?: "md" | "lg";
+}
+
+const TextButton: React.FC<TextButtonProps> = ({
+  children,
+  color = "default",
+  size = "md",
+  className,
+}) => {
+  return (
+    <TextButtonBox color={color} size={size} className={className}>
+      <TextButtonSpan>{children}</TextButtonSpan>
+    </TextButtonBox>
+  );
+};
+
+export default TextButton;
