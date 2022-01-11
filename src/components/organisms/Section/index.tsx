6c4e5bc7193342567aece93ae557a40cb40e2010
dohpark <parkdohee19931129@gmail.com>
2022-01-12 01:03:49 +0900

    feat: 추가 Section organism
    Card, Carousel의 molecule을 하나로 뭉칠 수 있도록 함

---
+++ src/components/organisms/Section/index.tsx
@@ -0,0 +1,57 @@
+import styled from "styled-components";
+import palette from "../../../styles/palette";
+import TextButton from "../../atoms/TextButton";
+
+const SectionContainer = styled.div`
+  margin-top: 72px;
+`;
+
+const Header = styled.div`
+  margin: auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+  margin-bottom: 20px;
+`;
+
+const Title = styled.h2`
+  font-size: 24px;
+  font-weight: bold;
+  margin: 0px;
+`;
+
+const SubTitle = styled.p`
+  font-size: 14px;
+  margin-top: 4px;
+  margin-bottom: 0px;
+  color: ${palette.gray600};
+`;
+
+interface SectionProps {
+  title: string;
+  subTitle?: string;
+  button?: boolean;
+  children: React.ReactNode;
+}
+
+const Section: React.FC<SectionProps> = ({
+  title,
+  subTitle,
+  button,
+  children,
+}) => {
+  return (
+    <SectionContainer>
+      <Header>
+        <div>
+          <Title>{title}</Title>
+          {subTitle && <SubTitle>{subTitle}</SubTitle>}
+        </div>
+        {button && <TextButton size="md">전체 클래스 보기</TextButton>}
+      </Header>
+      {children}
+    </SectionContainer>
+  );
+};
+
+export default Section;
