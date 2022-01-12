3b2985ec1123571bc35c14b0fd30f04fe43acf32
dohpark <parkdohee19931129@gmail.com>
2022-01-13 00:59:11 +0900

    feat: 추가 Menu Icon

--- src/components/atoms/Icon/index.tsx
+++ src/components/atoms/Icon/index.tsx
@@ -8,7 +8,8 @@ type iconName =
   | "LikeOutline"
   | "Bell"
   | "Search"
-  | "Logo";
+  | "Logo"
+  | "Menu";
 
 type icon = {
   path: string;
@@ -54,6 +55,10 @@ const IconSet: Record<iconName, icon> = {
   },
   Logo: {
     path: "M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z",
+    viewBox: "0 0 24 24",
+  },
+  Menu: {
+    path: "M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z",
     viewBox: "0 0 24 24",
   },
 };
