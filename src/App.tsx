3b3a1e9d43d7728b53b17592a3bef540cf16e7e2
dohpark <parkdohee19931129@gmail.com>
2022-01-12 18:50:18 +0900

    feat: 추가 Header organism
    로고, 서치바 및 버튼 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,95 +1,86 @@
-import styled from "styled-components";
-import Button from "./components/atoms/Button";
-import Icon from "./components/atoms/Icon";
-import IconButton from "./components/atoms/IconButton";
 import Card from "./components/molecules/Card";
 import Carousel from "./components/molecules/Carousel";
-import palette from "./styles/palette";
 import { timeDeal, popularEvent, mdRecommend, openSoon } from "./data/data";
-
-const Slide = styled.div`
-  width: 100%;
-  height: 300px;
-  background-color: brown;
-`;
+import Section from "./components/organisms/Section";
+import Header from "./components/organisms/Header";
 
 function App() {
   return (
     <div className="App">
-      <Icon size={100} iconName={"Heart"} />
-      <Button>help</Button>
-      <IconButton
-        iconName={"Like"}
-        fillColor={palette.orange400}
-        backgroundColor="transparent"
-      />
-      <IconButton
-        iconName={"LikeOutline"}
-        fillColor={palette.orange400}
-        backgroundColor="transparent"
-      />
-      <Carousel slidesView={3} navPosition="rightIn" iconColor="white">
-        {[1, 2, 3, 4, 5].map((order) => (
-          <Slide key={order} />
-        ))}
-      </Carousel>
-      <Carousel slidesView={4} navPosition="eachSide" iconColor="black">
-        {timeDeal.time_deal.map(
-          ({ id, title, creator, img, like, thumsUp, price, coupon }) => (
+      <Header />
+      <Section title={"오늘의 특가! TIME DEAL"} button={true}>
+        <Carousel slidesView={4} navPosition="eachSide" iconColor="black">
+          {timeDeal.time_deal.map(
+            ({ id, title, creator, img, like, thumsUp, price, coupon }) => (
+              <Card
+                type="timeDeal"
+                key={id}
+                title={title}
+                creator={creator}
+                img={img}
+                like={like}
+                thumsUp={thumsUp}
+                price={price}
+                coupon={coupon}
+              />
+            )
+          )}
+        </Carousel>
+      </Section>
+      <Section title={"MD 추천 클래스"}>
+        <Carousel slidesView={4} navPosition="eachSide" iconColor="black">
+          {mdRecommend.md_recommend.map(
+            ({ id, title, creator, img, like, thumsUp, price, coupon }) => (
+              <Card
+                type="mdRecommend"
+                key={id}
+                title={title}
+                creator={creator}
+                img={img}
+                like={like}
+                thumsUp={thumsUp}
+                price={price}
+                coupon={coupon}
+              />
+            )
+          )}
+        </Carousel>
+      </Section>
+      <Section
+        title={"오픈 예정 클래스"}
+        subTitle={"오픈 예정인 클래스를 응원하면 얼리버드 오픈 시 알려드려요!"}
+        button={true}
+      >
+        <Carousel slidesView={4} navPosition="eachSide" iconColor="black">
+          {openSoon.open_soon.map(({ id, title, creator, img, cheer }) => (
             <Card
-              type="timeDeal"
+              type="openSoon"
               key={id}
               title={title}
               creator={creator}
               img={img}
-              like={like}
-              thumsUp={thumsUp}
-              price={price}
-              coupon={coupon}
+              cheer={cheer}
             />
-          )
-        )}
-      </Carousel>
-      <Carousel slidesView={4} navPosition="eachSide" iconColor="black">
-        {mdRecommend.md_recommend.map(
-          ({ id, title, creator, img, like, thumsUp, price, coupon }) => (
+          ))}
+        </Carousel>
+      </Section>
+      <Section
+        title={"인기있는 신규 클래스"}
+        subTitle={"얼리버드 기간에만 받을 수 있는 최저가 할인 중이에요"}
+        button={true}
+      >
+        <Carousel slidesView={3} navPosition="eachSide" iconColor="black">
+          {popularEvent.popular_event.map(({ id, title, img, period }) => (
             <Card
-              type="mdRecommend"
+              type="popularEvent"
               key={id}
               title={title}
-              creator={creator}
               img={img}
-              like={like}
-              thumsUp={thumsUp}
-              price={price}
-              coupon={coupon}
+              period={period}
             />
-          )
-        )}
-      </Carousel>
-      <Carousel slidesView={4} navPosition="eachSide" iconColor="black">
-        {openSoon.open_soon.map(({ id, title, creator, img, cheer }) => (
-          <Card
-            type="openSoon"
-            key={id}
-            title={title}
-            creator={creator}
-            img={img}
-            cheer={cheer}
-          />
-        ))}
-      </Carousel>
-      <Carousel slidesView={3} navPosition="eachSide" iconColor="black">
-        {popularEvent.popular_event.map(({ id, title, img, period }) => (
-          <Card
-            type="popularEvent"
-            key={id}
-            title={title}
-            img={img}
-            period={period}
-          />
-        ))}
-      </Carousel>
+          ))}
+        </Carousel>
+      </Section>
     </div>
   );
 }
---
+++ src/components/organisms/Header/index.tsx
@@ -0,0 +1,102 @@
+import styled from "styled-components";
+import Button from "../../atoms/Button";
+import palette from "../../../styles/palette";
+import SearchBar from "../../molecules/SearchBar";
+
+const HeaderContainer = styled.header`
+  max-width: 1176px;
+  width: 100%;
+  padding: 20px 0;
+  margin-left: auto;
+  margin-right: auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const LogoWrapper = styled.div`
+  width: 12.5%;
+  font-size: 1.5rem;
+  font-weight: 800;
+`;
+
+const ClassStoreWrapper = styled.div`
+  display: flex;
+  width: 12.5%;
+  margin-right: 36px;
+
+  .class {
+    justify-content: start;
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 1.25rem;
+    font-weight: 700;
+    color: ${palette.orange700};
+  }
+
+  .store {
+    justify-content: start;
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 1.25rem;
+    font-weight: 700;
+    color: ${palette.black};
+  }
+`;
+
+const SearchWrapper = styled.div`
+  width: 35%;
+`;
+
+const ButtonWrapper = styled.div`
+  width: 40%;
+  display: flex;
+
+  .space {
+    width: 41%;
+    height: 1px;
+  }
+  .buttons {
+    display: flex;
+    flex-grow: 1;
+  }
+
+  .creatorsupport,
+  .corpedu,
+  .login {
+    font-weight: 400;
+    justify-content: end;
+    font-size: 14px;
+    margin: 0;
+    border: 0;
+    padding: 0;
+    color: ${palette.gray800};
+  }
+`;
+
+const Header = () => {
+  return (
+    <HeaderContainer>
+      <LogoWrapper>CLASS102</LogoWrapper>
+      <ClassStoreWrapper>
+        <Button className="class">클래스</Button>
+        <Button className="store">스토어</Button>
+      </ClassStoreWrapper>
+      <SearchWrapper>
+        <SearchBar placeholder={"찾으시는 취미가 있으신가요?"} />
+      </SearchWrapper>
+      <ButtonWrapper>
+        <div className="space"></div>
+        <div className="buttons">
+          <Button className="creatorsupport">크리에이터 지원</Button>
+          <Button className="corpedu">기업교육</Button>
+          <Button className="login">로그인</Button>
+        </div>
+      </ButtonWrapper>
+    </HeaderContainer>
+  );
+};
+
+export default Header;
