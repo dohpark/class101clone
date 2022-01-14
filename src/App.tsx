76167b0be430eaf9fafaba8ba101bc999464df27
dohpark <parkdohee19931129@gmail.com>
2022-01-14 16:42:23 +0900

    feat: 추가 progress bar
    banner에 progress bar 추가
    
    progress bar 키고 끄기 기능을 하기 위해서 carousel과 연결이 필요해보임

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,63 @@
+import styled from "styled-components";
 import Card from "./components/molecules/Card";
 import Carousel from "./components/molecules/Carousel";
-import { timeDeal, popularEvent, mdRecommend, openSoon } from "./data/data";
+import {
+  timeDeal,
+  popularEvent,
+  mdRecommend,
+  openSoon,
+  topEvent,
+  bottomEvent,
+} from "./data/data";
 import Section from "./components/organisms/Section";
 import Header from "./components/organisms/Header";
 import Nav from "./components/organisms/Nav";
+import Banner from "./components/molecules/Banner";
+import ProgressBar from "./components/atoms/ProgressBar";
+import BannerLarge from "./components/molecules/Banner copy";
+
+const BannerContainer = styled.div`
+  margin-top: 72px;
+`;
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Nav />
+      <BannerLarge
+        title={"hello"}
+        subtitle={"subtitle"}
+        img={
+          "https://images.velog.io/images/kados22/post/0e43ee9f-3ecf-46af-9885-b70a5a78ed2e/%E1%84%8B%E1%85%B5%E1%86%AF%E1%84%87%E1%85%A1%E1%86%AB%20Class%20Card%20%E1%84%8A%E1%85%A5%E1%86%B7%E1%84%82%E1%85%A6%E1%84%8B%E1%85%B5%E1%86%AF%203.png"
+        }
+        bgColor="red"
+        type="top"
+      ></BannerLarge>
+      <BannerContainer>
+        <Carousel
+          slidesView={1}
+          navPosition="rightIn"
+          iconColor="black"
+          iconBackgroundColor="transparent"
+          paginationType="number"
+          autoplay={true}
+        >
+          {topEvent.top_event.map(
+            ({ id, title, subtitle, img, bgColor, badge }) => (
+              <Banner
+                type="top"
+                key={id}
+                title={title}
+                subtitle={subtitle}
+                img={img}
+                bgColor={bgColor}
+                badge={badge}
+              />
+            )
+          )}
+        </Carousel>
+      </BannerContainer>
       <Section title={"오늘의 특가! TIME DEAL"} button={true}>
         <Carousel slidesView={4} navPosition="eachSide" iconColor="black">
           {timeDeal.time_deal.map(
@@ -48,6 +96,28 @@ function App() {
           )}
         </Carousel>
       </Section>
+      <BannerContainer>
+        <Carousel
+          slidesView={1}
+          navPosition="rightIn"
+          iconColor="black"
+          iconBackgroundColor="white"
+          paginationType="circle"
+        >
+          {bottomEvent.bottom_event.map(
+            ({ id, title, subtitle, img, bgColor }) => (
+              <Banner
+                type="bottom"
+                key={id}
+                title={title}
+                subtitle={subtitle}
+                img={img}
+                bgColor={bgColor}
+              />
+            )
+          )}
+        </Carousel>
+      </BannerContainer>
       <Section
         title={"오픈 예정 클래스"}
         subTitle={"오픈 예정인 클래스를 응원하면 얼리버드 오픈 시 알려드려요!"}
--- src/components/molecules/Banner/index.tsx
+++ src/components/molecules/Banner/index.tsx
@@ -1,70 +1,152 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import palette from "../../../styles/palette";
 import Image from "../../atoms/Image";
+import ProgressBar from "../../atoms/ProgressBar";
+import Pagination from "../Pagination";
 
 interface StyledBannerContainer {
   bgColor: string;
+  type: "bottom" | "top";
 }
 
-const BannerBackground = styled.div`
-  padding-top: 34px;
+interface StyledType {
+  type: "bottom" | "top";
+}
+
+const BannerBackground = styled.div<StyledType>`
+  ${(props) => {
+    if (props.type === "bottom")
+      return css`
+        padding-top: 34px;
+      `;
+    else if (props.type === "top")
+      return css`
+        padding-bottom: 34px;
+      `;
+  }}
 `;
 
 const BannerContainer = styled.div<StyledBannerContainer>`
-  background-color: ${(props) => props.bgColor};
-  display: flex;
-  flex-wrap: wrap-reverse;
-  height: 210px;
+  ${(props) => {
+    if (props.type === "bottom")
+      return css`
+        background-color: ${props.bgColor};
+        display: flex;
+        flex-wrap: wrap-reverse;
+        height: 210px;
+      `;
+    else if (props.type === "top")
+      return css`
+        background-color: ${props.bgColor};
+      `;
+  }}
 `;
 
-const BannerWrapper = styled.div`
+const BannerWrapper = styled.div<StyledType>`
   max-width: 1176px;
   width: 100%;
   margin-left: auto;
   margin-right: auto;
   display: flex;
-  justify-content: space-between;
-  position: relative;
+  ${(props) => {
+    if (props.type === "bottom")
+      return css`
+        justify-content: space-between;
+        position: relative;
+      `;
+    else if (props.type === "top")
+      return css`
+        height: 450px;
+      `;
+  }}
+`;
 
-  .textBox {
-    display: flex;
-    align-items: left;
-    flex-direction: column;
-    justify-content: center;
-  }
+const ImageWrapper = styled.div<StyledType>`
+  ${(props) => {
+    if (props.type === "bottom")
+      return css`
+        position: absolute;
+        right: 0;
+        bottom: -1px;
+        display: block;
+        height: 244px;
+      `;
+    else if (props.type === "top")
+      return css`
+        width: 58%;
+        position: relative;
+        top: 35px;
+      `;
+  }}
+`;
 
-  .title {
-    font-size: 32px;
-    font-weight: bold;
-    margin: 0px;
-    color: ${palette.white};
-  }
+const TextWrapper = styled.div<StyledType>`
+  ${(props) => {
+    if (props.type === "bottom")
+      return css`
+        display: flex;
+        align-items: left;
+        flex-direction: column;
+        justify-content: center;
 
-  .subtitle {
-    font-size: 14px;
-    margin: 6px 0px 0px;
-    color: ${palette.white};
-  }
+        .title {
+          font-size: 32px;
+          font-weight: bold;
+          margin: 0px;
+          color: ${palette.white};
+        }
 
-  .button {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin-top: 24px;
-    font-size: 14px;
-    height: 32px;
-    width: 90px;
-    border-radius: 3px;
-    background-color: ${palette.white};
-  }
+        .subtitle {
+          font-size: 14px;
+          margin: 6px 0px 0px;
+          color: ${palette.white};
+        }
 
-  .imageWrapper {
-    position: absolute;
-    right: 0;
-    bottom: -1px;
-    display: block;
-    height: 244px;
-  }
+        .button {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          margin-top: 24px;
+          font-size: 14px;
+          height: 32px;
+          width: 90px;
+          border-radius: 3px;
+          background-color: ${palette.white};
+        }
+      `;
+    else if (props.type === "top")
+      return css`
+        width: 42%;
+        background-color: transparent;
+        padding-left: 48px;
+        padding-bottom: 32px;
+
+        .text {
+          padding-top: 56px;
+        }
+
+        .title {
+          display: flex;
+          max-width: 380px;
+          align-items: center;
+          font-size: 34px;
+          font-weight: bold;
+          color: ${palette.white};
+        }
+
+        .subtitle {
+          margin: 10px 0 0;
+          color: ${palette.gray100};
+          font-weight: 500;
+          max-width: 300px;
+        }
+
+        .number {
+          position: relative;
+          transition: none;
+        }
+      `;
+  }}
 `;
 
 interface BannerProps {
@@ -73,6 +155,11 @@ interface BannerProps {
   img: string;
   badge?: string;
   bgColor: string;
+  type: "top" | "bottom";
+  slidesView?: number;
+  active?: number;
+  count?: number;
+  onClickPaginationHandler?: (index: number) => void;
 }
 
 const Banner: React.FC<BannerProps> = ({
@@ -81,20 +168,53 @@ const Banner: React.FC<BannerProps> = ({
   img,
   badge,
   bgColor,
+  type,
+  slidesView = 0,
+  active = 0,
+  count = 0,
+  onClickPaginationHandler = () => {},
 }) => {
+  if (bgColor === "") {
+    bgColor = `${palette.gray700}`;
+  }
+
   return (
-    <BannerBackground>
-      <BannerContainer bgColor={bgColor}>
-        <BannerWrapper>
-          <div className="textBox">
-            <div className="title">{title}</div>
-            <div className="subtitle">{subtitle}</div>
-            <div className="button">더 알아보기</div>
-          </div>
-          <div className="imageWrapper">
-            <Image src={img} alt={title} expand={false} />
-          </div>
-        </BannerWrapper>
+    <BannerBackground type={type}>
+      <BannerContainer bgColor={bgColor} type={type}>
+        {type === "top" && (
+          <BannerWrapper type={type}>
+            <ImageWrapper type={type}>
+              <Image src={img} alt={title} expand={false} />
+            </ImageWrapper>
+            <TextWrapper type={type}>
+              <div className="text">
+                <p className="title">{title}</p>
+                <p className="subtitle">{subtitle}</p>
+              </div>
+              <Pagination
+                className="number"
+                paginationType="number"
+                slidesPerView={slidesView}
+                active={active}
+                childrenCount={count}
+                onClickPaginationHandler={onClickPaginationHandler!}
+              />
+              <ProgressBar />
+            </TextWrapper>
+          </BannerWrapper>
+        )}
+        {type === "bottom" && (
+          <BannerWrapper type={type}>
+            <TextWrapper type={type}>
+              <div className="title">{title}</div>
+              <div className="subtitle">{subtitle}</div>
+              <div className="button">더 알아보기</div>
+            </TextWrapper>
+            <ImageWrapper type={type}>
+              <Image src={img} alt={title} expand={false} />
+            </ImageWrapper>
+          </BannerWrapper>
+        )}
       </BannerContainer>
     </BannerBackground>
   );
