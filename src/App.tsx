9d32b8aaafe94b0cbb98499398c71fe4a53c8313
dohpark <parkdohee19931129@gmail.com>
2022-01-31 18:25:54 +0900

    style: GlobalStyle delete

--- src/App.tsx
+++ src/App.tsx
@@ -16,8 +16,6 @@ import BottomNav from "./components/organisms/BottomNav";
 import CarouselBottomBanner from "./components/organisms/CarouselBottomBanner";
 import CarouselCard from "./components/organisms/CarouselCard";
 import CarouselTopBanner from "./components/organisms/CarouselTopBanner";
-import React from "react";
-import GlobalStyle from "./styles/GlobalStyle";
 
 const HomePage = styled.div``;
 
@@ -27,105 +25,100 @@ const BannerContainer = styled.div`
 
 function App() {
   return (
-    <React.Fragment>
-      <GlobalStyle />
-      <HomePage>
-        <Header />
-        <Nav />
-        <CarouselTopBanner array={topEvent.top_event} />
-        <Section title={"오늘의 특가! TIME DEAL"} button={true}>
-          <CarouselCard slidesPerView={4} mobileSlidesPerView={2}>
-            {timeDeal.time_deal.map(
-              ({ id, title, creator, img, like, thumsUp, price, coupon }) => (
-                <Card
-                  type="timeDeal"
-                  key={id}
-                  title={title}
-                  creator={creator}
-                  img={img}
-                  like={like}
-                  thumsUp={thumsUp}
-                  price={price}
-                  coupon={coupon}
-                />
-              )
-            )}
-          </CarouselCard>
-        </Section>
-        <Section title={"MD 추천 클래스"}>
-          <CarouselCard slidesPerView={4} mobileSlidesPerView={2}>
-            {mdRecommend.md_recommend.map(
-              ({ id, title, creator, img, like, thumsUp, price, coupon }) => (
-                <Card
-                  type="mdRecommend"
-                  key={id}
-                  title={title}
-                  creator={creator}
-                  img={img}
-                  like={like}
-                  thumsUp={thumsUp}
-                  price={price}
-                  coupon={coupon}
-                />
-              )
-            )}
-          </CarouselCard>
-        </Section>
-        <BannerContainer>
-          <CarouselBottomBanner slidesPerView={1} mobileSlidesPerView={1}>
-            {bottomEvent.bottom_event.map(
-              ({ id, title, subtitle, img, bgColor }) => (
-                <Banner
-                  key={id}
-                  title={title}
-                  subtitle={subtitle}
-                  img={img}
-                  bgColor={bgColor}
-                />
-              )
-            )}
-          </CarouselBottomBanner>
-        </BannerContainer>
-        <Section
-          title={"오픈 예정 클래스"}
-          subTitle={
-            "오픈 예정인 클래스를 응원하면 얼리버드 오픈 시 알려드려요!"
-          }
-          button={true}
-        >
-          <CarouselCard slidesPerView={4} mobileSlidesPerView={2}>
-            {openSoon.open_soon.map(({ id, title, creator, img, cheer }) => (
+    <HomePage>
+      <Header />
+      <Nav />
+      <CarouselTopBanner array={topEvent.top_event} />
+      <Section title={"오늘의 특가! TIME DEAL"} button={true}>
+        <CarouselCard slidesPerView={4} mobileSlidesPerView={2}>
+          {timeDeal.time_deal.map(
+            ({ id, title, creator, img, like, thumsUp, price, coupon }) => (
               <Card
-                type="openSoon"
+                type="timeDeal"
                 key={id}
                 title={title}
                 creator={creator}
                 img={img}
-                cheer={cheer}
+                like={like}
+                thumsUp={thumsUp}
+                price={price}
+                coupon={coupon}
               />
-            ))}
-          </CarouselCard>
-        </Section>
-        <Section
-          title={"인기있는 신규 클래스"}
-          subTitle={"얼리버드 기간에만 받을 수 있는 최저가 할인 중이에요"}
-          button={true}
-        >
-          <CarouselCard slidesPerView={3} mobileSlidesPerView={1}>
-            {popularEvent.popular_event.map(({ id, title, img, period }) => (
+            )
+          )}
+        </CarouselCard>
+      </Section>
+      <Section title={"MD 추천 클래스"}>
+        <CarouselCard slidesPerView={4} mobileSlidesPerView={2}>
+          {mdRecommend.md_recommend.map(
+            ({ id, title, creator, img, like, thumsUp, price, coupon }) => (
               <Card
-                type="popularEvent"
+                type="mdRecommend"
                 key={id}
                 title={title}
+                creator={creator}
                 img={img}
-                period={period}
+                like={like}
+                thumsUp={thumsUp}
+                price={price}
+                coupon={coupon}
               />
-            ))}
-          </CarouselCard>
-        </Section>
-        <BottomNav />
-      </HomePage>
-    </React.Fragment>
+            )
+          )}
+        </CarouselCard>
+      </Section>
+      <BannerContainer>
+        <CarouselBottomBanner slidesPerView={1} mobileSlidesPerView={1}>
+          {bottomEvent.bottom_event.map(
+            ({ id, title, subtitle, img, bgColor }) => (
+              <Banner
+                key={id}
+                title={title}
+                subtitle={subtitle}
+                img={img}
+                bgColor={bgColor}
+              />
+            )
+          )}
+        </CarouselBottomBanner>
+      </BannerContainer>
+      <Section
+        title={"오픈 예정 클래스"}
+        subTitle={"오픈 예정인 클래스를 응원하면 얼리버드 오픈 시 알려드려요!"}
+        button={true}
+      >
+        <CarouselCard slidesPerView={4} mobileSlidesPerView={2}>
+          {openSoon.open_soon.map(({ id, title, creator, img, cheer }) => (
+            <Card
+              type="openSoon"
+              key={id}
+              title={title}
+              creator={creator}
+              img={img}
+              cheer={cheer}
+            />
+          ))}
+        </CarouselCard>
+      </Section>
+      <Section
+        title={"인기있는 신규 클래스"}
+        subTitle={"얼리버드 기간에만 받을 수 있는 최저가 할인 중이에요"}
+        button={true}
+      >
+        <CarouselCard slidesPerView={3} mobileSlidesPerView={1}>
+          {popularEvent.popular_event.map(({ id, title, img, period }) => (
+            <Card
+              type="popularEvent"
+              key={id}
+              title={title}
+              img={img}
+              period={period}
+            />
+          ))}
+        </CarouselCard>
+      </Section>
+      <BottomNav />
+    </HomePage>
   );
 }
 
